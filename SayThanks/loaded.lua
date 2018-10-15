0d700c1539eb3ec1f20e2543fc91ab8f0a3722ab
l0mak <dmitry.l0mak@gmail.com>
2018-10-15 15:10:16 +0300

    Add files via upload

---
+++ SayThanks/loaded.lua
@@ -0,0 +1,6 @@
+-- This file is loaded from "SayThanks.toc"
+
+----------------------------------------------------------------------------------------
+--	Says thanks for using Saythanks Addon
+----------------------------------------------------------------------------------------
+print("Thanks for using SayThanks Addon!")
