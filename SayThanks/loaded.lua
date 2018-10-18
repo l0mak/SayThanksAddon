29debde65d66e407397d04371640a9dbaed24f7b
Dmitry l0mak <dmitry.l0mak@gmail.com>
2018-10-19 02:21:12 +0300

    Update loaded.lua

--- SayThanks/loaded.lua
+++ SayThanks/loaded.lua
@@ -1,6 +1,2 @@
--- This file is loaded from "SayThanks.toc"
-
-----------------------------------------------------------------------------------------
 --	Says thanks for using Saythanks Addon
-----------------------------------------------------------------------------------------
 print("Thanks for using SayThanks Addon!")
